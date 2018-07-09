193451ef7cdecdd85134bbdd4d43b0fd43435be6
Aman Rawat <31853960+AmanRawat882@users.noreply.github.com>
2018-07-09 23:26:10 +0530

    Icon

--- erpnext/config/desktop.py
+++ erpnext/config/desktop.py
@@ -1,14 +1,31 @@
-# -*- coding: utf-8 -*-
+# coding=utf-8
+
 from __future__ import unicode_literals
 from frappe import _
 
 def get_data():
 	return [
 		{
-			"module_name": "erpnext",
-			"color": "grey",
-			"icon": "octicon octicon-file-directory",
+			"module_name": "Project",
+			"_doctype": "Project",
+			"color": "#8e44ad",
+			"icon": "octicon octicon-rocket",
+			"type": "link",
+			"link": "List/Project"
+		},
+		{
+			"module_name": "HR",
+			"color": "#2ecc71",
+			"icon": "octicon octicon-organization",
+			"label": _("Human Resources"),
 			"type": "module",
-			"label": _("erpnext")
+			"hidden": 1
+		},
+		{
+			"module_name": "Projects",
+			"color": "#8e44ad",
+			"icon": "octicon octicon-rocket",
+			"type": "module",
+			"hidden": 1
 		}
 	]
