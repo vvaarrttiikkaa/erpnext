a9610a2c195d8577ab6b50a2e6b8a8a243e444c0
Abhimanyu <abhimanyudwivedi@outlook.com>
2018-07-10 10:54:33 +0530

    Script changes

--- erpnext/hr/doctype/expense_claim_type/expense_claim_type.js
+++ erpnext/hr/doctype/expense_claim_type/expense_claim_type.js
@@ -1,5 +1,10 @@
 // Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 // License: GNU General Public License v3. See license.txt
+frappe.ui.form.on('Employee Grade', {
+	refresh: function(frm) {
+
+	}
+});
 
 frappe.ui.form.on("Expense Claim Type", {
 	refresh: function(frm){
