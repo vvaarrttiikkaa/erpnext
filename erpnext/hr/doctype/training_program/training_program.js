f895dfef99950f32d74176bf9232cdd41085c700
Aman Rawat <31853960+AmanRawat882@users.noreply.github.com>
2018-07-10 08:03:06 +0530

    Scripting

--- erpnext/hr/doctype/training_program/training_program.js
+++ erpnext/hr/doctype/training_program/training_program.js
@@ -1,8 +1,5 @@
-// Copyright (c) 2018, Frappe Technologies and contributors
+// Copyright (c) 2017, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('Training Program', {
-	refresh: function(frm) {
-
-	}
 });
