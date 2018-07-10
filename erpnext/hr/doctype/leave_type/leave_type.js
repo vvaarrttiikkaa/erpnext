50a3bfe4384f8f7673064a3c00a2745cb7e949fa
Aman Rawat <31853960+AmanRawat882@users.noreply.github.com>
2018-07-10 10:55:01 +0530

    Scripting

--- erpnext/hr/doctype/leave_type/leave_type.js
+++ erpnext/hr/doctype/leave_type/leave_type.js
@@ -1,8 +1,8 @@
-// Copyright (c) 2018, Frappe Technologies and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Leave Type', {
+frappe.ui.form.on("Leave Type", {
 	refresh: function(frm) {
-
+		frm.add_custom_button(__("Allocations"), function() {
+			frappe.set_route("List", "Leave Allocation",
+			{"leave_type": frm.doc.name});
+		});
 	}
 });
--- erpnext/hr/doctype/loan_type/loan_type.js
+++ erpnext/hr/doctype/loan_type/loan_type.js
@@ -1,8 +1,7 @@
-// Copyright (c) 2018, Frappe Technologies and contributors
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('Loan Type', {
 	refresh: function(frm) {
-
 	}
 });
